827ec079bdb8b3402f64eaa23f9268664c78d9e6
Flatline01 <>
2025-03-17 00:35:57 +0100

    [any message]

--- app/main.go
+++ app/main.go
@@ -4,16 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		user_command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		user_command, err := reader.ReadString('\n')
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println(user_command[:len(user_command)-1] + ": command not found")
+
+		user_command = strings.TrimSpace(user_command)
+		switch user_command {
+		case "exit 0":
+			os.Exit(0)
+		default:
+			fmt.Printf("%s: command not found\n", user_command)
+		}
 	}
+
 }
