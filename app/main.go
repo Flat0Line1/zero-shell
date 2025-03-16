67f00cc4cbb60b7dcaef9f36b11fbe9020ee9a48
Flatline01 <>
2025-03-17 00:10:29 +0100

    [any message]

--- app/main.go
+++ app/main.go
@@ -6,13 +6,14 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-
 func main() {
 	// Uncomment this block to pass the first stage
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	user_command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(user_command[:len(user_command)-1] + ": command not found")
 }
