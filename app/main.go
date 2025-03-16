db0319c33ad94f135950490c59b90f3bbdfb9939
Flatline01 <>
2025-03-17 00:24:26 +0100

    [any message]

--- app/main.go
+++ app/main.go
@@ -7,13 +7,13 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	user_command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		panic(err)
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		user_command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(user_command[:len(user_command)-1] + ": command not found")
 	}
-	fmt.Println(user_command[:len(user_command)-1] + ": command not found")
 }
